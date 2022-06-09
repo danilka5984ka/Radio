eff4be39ee5eccc770e56f92002c6abc8656e7bc
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-06-09 20:09:50 +0700

    Plugin Lombok

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
 
     private int maxChanel = 9;
@@ -9,86 +17,5 @@ public class Radio {
     private int maxVolume = 100;
     private int minVolume = 0;
     private int currentVolume;
-
-    public Radio(int minChanel, int maxChanel) {
-        this.maxChanel = maxChanel;
-        this.minChanel = minChanel;
-        this.currentChanel = minChanel;
-    }
-
-    public Radio(int size) {
-        maxChanel = minChanel + size;
-
-    }
-
-    public Radio() {
-    }
-
-    public int getCurrentChanel() {
-        return currentChanel;
-    }
-
-    public int getMaxChanel() {
-        return maxChanel;
-    }
-
-    public int getMinChanel() {
-        return minChanel;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentChanel(int currentChanel) {
-
-        if (currentChanel < minChanel) {
-            currentChanel = maxChanel;
-        }
-        if (currentChanel > maxChanel) {
-            currentChanel = minChanel;
-        }
-        this.currentChanel = currentChanel;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-
-        if (currentVolume < minVolume) {
-            currentVolume = minVolume;
-        }
-        if (currentVolume > maxVolume) {
-            currentVolume = maxVolume;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void next() {
-        int nextChanel = currentChanel + 1;
-        setCurrentChanel(nextChanel);
-    }
-
-    public void prev() {
-        int prevChanel = currentChanel - 1;
-        setCurrentChanel(prevChanel);
-    }
-
-    public void increaseVolume(int currentVolume) {
-        if (currentVolume < maxVolume) {
-            currentVolume = currentVolume + 1;
-        } else {
-            currentVolume = maxVolume;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void decreaseVolume(int currentVolume) {
-
-        if (currentVolume > minVolume) {
-            currentVolume = currentVolume - 1;
-        } else {
-            currentVolume = minVolume;
-        }
-        this.currentVolume = currentVolume;
-    }
 
 }
