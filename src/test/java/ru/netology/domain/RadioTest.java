07c561cfb7eca3ced513cf99fdb078c0d289eb93
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-06-09 20:32:25 +0700

    Reload plugin Lombok

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -3,8 +3,6 @@ package ru.netology.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RadioTest {
 
     @Test
