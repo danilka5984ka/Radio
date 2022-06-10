4a176aeafdb4274679d3dcdf81834c7bd713d1c2
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-06-10 16:08:23 +0700

    Revert "Plugin Lombok (reload)"
    This reverts commit b82781b0b45918739a4445880a3304c30e72d034.

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,6 @@
 package ru.netology.domain;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,79 +14,8 @@ public class Radio {
     private int minChanel = 0;
     private int currentChanel = minChanel;
 
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
-
-    public Radio(int minChanel, int maxChanel) {
-        this.maxChanel = maxChanel;
-        this.minChanel = minChanel;
-        this.currentChanel = minChanel;
-    }
-
-    public int getCurrentChanel() {
-        return currentChanel;
-    }
-
-    public int getMaxChanel() {
-        return maxChanel;
-    }
-
-    public int getMinChanel() {
-        return minChanel;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentChanel(int currentChanel) {
-
-        if (currentChanel < minChanel) {
-            currentChanel = maxChanel;
-        }
-        if (currentChanel > maxChanel) {
-            currentChanel = minChanel;
-        }
-        this.currentChanel = currentChanel;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-
-        if (currentVolume < 0) {
-            currentVolume = 0;
-        }
-        if (currentVolume > 100) {
-            currentVolume = 100;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void next() {
-        int nextChanel = currentChanel + 1;
-        setCurrentChanel(nextChanel);
-    }
-
-    public void prev() {
-        int prevChanel = currentChanel - 1;
-        setCurrentChanel(prevChanel);
-    }
-
-    public void increaseVolume(int currentVolume) {
-        if (currentVolume < 100) {
-            currentVolume = currentVolume + 1;
-        } else {
-            currentVolume = 100;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void decreaseVolume(int currentVolume) {
-
-        if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
-        } else {
-            currentVolume = 0;
-        }
-        this.currentVolume = currentVolume;
-    }
 
 }
